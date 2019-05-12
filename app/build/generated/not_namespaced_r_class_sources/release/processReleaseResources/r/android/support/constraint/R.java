a711d02c92de6dbb85f184e4eb7220ac7a066d3c
Sparrow <1158443037@qq.com>
2019-05-12 23:04:09 +0800

    BY 22j

--- app/build/generated/not_namespaced_r_class_sources/release/processReleaseResources/r/android/support/constraint/R.java
+++ app/build/generated/not_namespaced_r_class_sources/release/processReleaseResources/r/android/support/constraint/R.java
@@ -73,20 +73,20 @@ public final class R {
     public static final class id {
         private id() {}
 
-        public static final int bottom = 0x7f080032;
-        public static final int end = 0x7f08005c;
-        public static final int gone = 0x7f080069;
-        public static final int invisible = 0x7f08007e;
-        public static final int left = 0x7f080083;
-        public static final int packed = 0x7f080099;
-        public static final int parent = 0x7f08009b;
-        public static final int percent = 0x7f08009e;
-        public static final int right = 0x7f0800a3;
-        public static final int spread = 0x7f0800ce;
-        public static final int spread_inside = 0x7f0800cf;
-        public static final int start = 0x7f0800d4;
-        public static final int top = 0x7f0800ea;
-        public static final int wrap = 0x7f0800fa;
+        public static final int bottom = 0x7f080033;
+        public static final int end = 0x7f080065;
+        public static final int gone = 0x7f080073;
+        public static final int invisible = 0x7f080089;
+        public static final int left = 0x7f08008e;
+        public static final int packed = 0x7f0800a4;
+        public static final int parent = 0x7f0800a6;
+        public static final int percent = 0x7f0800a9;
+        public static final int right = 0x7f0800bc;
+        public static final int spread = 0x7f0800e9;
+        public static final int spread_inside = 0x7f0800ea;
+        public static final int start = 0x7f0800ef;
+        public static final int top = 0x7f08010b;
+        public static final int wrap = 0x7f08011d;
     }
     public static final class styleable {
         private styleable() {}
